2ae3b2dcdaed7d309a76882c357a16a2214f496a
bnabis93 <qhsh9713@gmail.com>
2020-01-01 22:08:10 +0900

    [Update] first commit strong connectivity component

---
+++ .ipynb_checkpoints/README-checkpoint.md
@@ -0,0 +1,2 @@
+## Algorithm  
+- Strong Connectivity Component (강한 연결 요소) 19.01.01
---
+++ strong_connectivity_component/2020_01_01_강연결요소.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+
+int main{
+    
+
+
+    return 0;
+}
